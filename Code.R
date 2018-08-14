e39959a226258af71f5491f13c91ad978845d33f
davislf2.net@gmail.com <davislf2.net@gmail.com>
2018-08-15 09:51:26 +1000

    update initial files

---
+++ Code.R
@@ -0,0 +1,31 @@
+
+1+3 # Run: cmd + enter
+# Comment: cmd + shift + C
+
+# Common functions
+# log2(4)
+# cos(2)
+
+# Two ways of help
+?log # Help of ?
+help(log) # Help of ?
+
+??sin
+log() # Auto-complete: Tab
+
+# Quick R, Cookbook for R
+# https://www.r-graph-gallery.com/
+# https://www.rstudio.com/wp-content/uploads/2016/10/r-cheat-sheet-3.pdf
+
+# Comparison
+1 == 2
+1 < 2
+1 <= 2
+1 > 2;
+
+# Variables
+x = 10 # inside/outside of functions
+y <- 10 # Assign operators: only outside of functions
+x+y
+# 1variable # Not allowed
+
